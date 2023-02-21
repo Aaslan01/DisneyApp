1806896192a1537a7e3b2e78a33b50c50715349b
Ali Sher Khan <72862093+Aaslan01@users.noreply.github.com>
2023-02-21 07:50:11 +0500

    Navigation.

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,21 +1,29 @@
-import { View, Text, Image, TouchableOpacity, SafeAreaView, } from 'react-native'
-import React from 'react'
+import {View, Text, Image, TouchableOpacity, SafeAreaView} from 'react-native';
+import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 import StyleGuide from '../constants/StyleGuide';
-import { ScrollView } from 'react-native-gesture-handler';
-import { trendingMovies, studios, movies } from '../data';
+import {ScrollView} from 'react-native-gesture-handler';
+import {trendingMovies, studios, movies} from '../data';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen'
+} from 'react-native-responsive-screen';
 import MoviesList from '../components/MoviesList';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
+
+const navigation = useNavigation()
   return (
-    <ScrollView style={{ backgroundColor: StyleGuide.colors.dark, flex: 1, paddingHorizontal: 16 }}>
+    <ScrollView
+      style={{
+        backgroundColor: StyleGuide.colors.dark,
+        flex: 1,
+        paddingHorizontal: 16,
+      }}>
       <Image
-        style={{ width: 100, height: 50, alignSelf: 'center' }}
-        source={require("../assets/images/logo.png")}
+        style={{width: 100, height: 50, alignSelf: 'center'}}
+        source={require('../assets/images/logo.png')}
       />
       <ScrollView
         horizontal
@@ -23,66 +31,54 @@ export default function HomeScreen() {
         snapToInterval={wp(90)}
         pagingEnabled
         decelerationRate={'fast'}
-        contentContainerStyle={{ paddingVertical: 10 }}
-      >
-        {
-          trendingMovies.map((movies) => (
-            <TouchableOpacity
-              key={movies.id}
-            >
-              <Image
-                style={{
-                  width: wp(90),
-                  height: hp(30),
-                  borderRadius: 10,
-                  overflow: 'hidden',
-                  marginRight: 10,
-                  resizeMode: 'contain'
-                }}
-                source={movies.image} />
-            </TouchableOpacity>
-          ))
-        }
+        contentContainerStyle={{paddingVertical: 10}}>
+        {trendingMovies.map(movies => (
+          <TouchableOpacity key={movies.id}>
+            <Image
+              style={{
+                width: wp(90),
+                height: hp(30),
+                borderRadius: 10,
+                overflow: 'hidden',
+                marginRight: 10,
+                resizeMode: 'contain',
+              }}
+              source={movies.image}
+            />
+          </TouchableOpacity>
+        ))}
       </ScrollView>
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
-      // contentContainerStyle={{ 
-      //   backgroundColor: StyleGuide.colors.darkBlue,
-      // }}
+        // contentContainerStyle={{
+        //   backgroundColor: StyleGuide.colors.darkBlue,
+        // }}
       >
-        {
-          studios.map((movies) => (
-            <TouchableOpacity
-              key={movies.id}
+        {studios.map(movies => (
+          <TouchableOpacity
+            key={movies.id}
+            style={{
+              width: wp(16),
+              height: hp(6),
+              borderRadius: 10,
+              marginHorizontal: 4,
+              backgroundColor: StyleGuide.colors.darkBlue,
+            }}>
+            <Image
               style={{
-                width: wp(16),
-                height: hp(6),
+                width: '100%',
+                height: '100%',
                 borderRadius: 10,
-                marginHorizontal: 4,
-                backgroundColor: StyleGuide.colors.darkBlue,
+                resizeMode: 'cover',
               }}
-            >
-              <Image
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: 10,
-                  resizeMode: 'cover'
-                }}
-                source={movies.image} />
-            </TouchableOpacity>
-          ))
-        }
+              source={movies.image}
+            />
+          </TouchableOpacity>
+        ))}
       </ScrollView>
-      <MoviesList 
-      movies={movies.slice(0,4)}
-      title='Recommendation for You'
-      />
-       <MoviesList 
-      title='Originals'
-      movies={movies.slice(5,9)}
-      />
+      <MoviesList movies={movies.slice(0, 4)} title="Recommendation for You" />
+      <MoviesList title="Originals" movies={movies.slice(5, 9)} />
     </ScrollView>
-  )
+  );
 }
--- components/Movies.tsx
+++ components/Movies.tsx
@@ -1,32 +1,33 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import { Movie as MovieType } from '../data'
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+import React from 'react';
+import {Movie as MovieType} from '../data';
 import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp,
-  } from 'react-native-responsive-screen'
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 interface Props {
-    movie: MovieType;
-    onPress?: () => void;
+  movie: MovieType;
+  onPress?: () => void;
+  navigation?: any;
 }
 
-const Movies: React.FC<Props> = ({ movie, onPress }) => {
-    return (
-        <TouchableOpacity
-            onPress={onPress}
-        >
-            <Image
-                resizeMode='contain'
-                style={{
-                    height: hp(20),
-                    width: wp(26),
-                    borderRadius: 6,
-                    marginHorizontal: wp(1)
-                }}
-                source={movie.image} />
-        </TouchableOpacity>
-    )
-}
 
-export default Movies
+const Movies: React.FC<Props> = ({movie, onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Image
+        resizeMode="contain"
+        style={{
+          height: hp(20),
+          width: wp(26),
+          borderRadius: 6,
+          marginHorizontal: wp(1),
+        }}
+        source={movie.image}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default Movies;
